4793f2e916b55deb50d5e1fcf285f0de7b99872e
Hai.nguyen <ng.hai0603@gmail.com>
2021-02-22 14:54:38 +0700

    up

--- 12.JDBC&CRUD/BT/ManagerUser/src/main/java/service/UserServiceIpml.java
+++ 12.JDBC&CRUD/BT/ManagerUser/src/main/java/service/UserServiceIpml.java
@@ -10,17 +10,19 @@ import java.util.List;
 public class UserServiceIpml implements UserService {
     ConService conService = new ConService();
 
-    protected final String SELECT_ALL = "SELECT user.*, country.name as countryName, country.id as countryId from user left join country on  user.country_id = country.id ";
+    protected final String SELECT_ALL = "call showAll()";
     protected final String INSERT_INTO = "call sp_createUser(?,?,?)";
-    protected final String UPDATE = "update user set name = ?, email = ?,country_id = ? where user.id = ?;";
+    protected final String UPDATE = "call sp_editUser(?,?,?,?)";
     protected final String SELECTBYID = "call FindById(?)";
-    protected final String DELETE_USER = "DELETE FROM user where id = ?";
+    protected final String DELETE_USER = "call sp_deleteUser(?)";
     protected final String SELECT_BY_COUNTRY = "SELECT user.*, country.name as countryName, country.id as countryId from user left join country on  user.country_id = country.id where country.name = ?";
     protected final String ORDER_BY_NAME = "select user.*, country.`name` as countryName, country.id as countryId from user left join country on user.country_id = country.id order by user.`name` desc ";
     @Override
     public List<User> showAll() throws SQLException {
         List<User> userList = new ArrayList<>();
-        try (PreparedStatement statement = conService.getConnection().prepareStatement(SELECT_ALL);) {
+        try {
+            conService.getConnection();
+            CallableStatement statement = conService.getConnection().prepareCall(SELECT_ALL);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -34,6 +36,8 @@ public class UserServiceIpml implements UserService {
                 userList.add(user);
 
             }
+        }catch (Exception e){
+            e.getMessage();
         }
         return userList;
     }
@@ -55,23 +59,31 @@ public class UserServiceIpml implements UserService {
 
     @Override
     public void editUser(int id, User data) throws SQLException {
-        try (PreparedStatement statement = conService.getConnection().prepareStatement(UPDATE)) {
+        try  {
+            conService.getConnection();
+            CallableStatement statement = conService.getConnection().prepareCall(UPDATE);
             statement.setString(1, data.getName());
             statement.setString(2, data.getEmail());
             statement.setInt(3, data.getCountry().getCountryID());
             statement.setInt(4, id);
 
             statement.executeUpdate();
+        }catch (Exception e){
+            e.getMessage();
         }
     }
 
     @Override
     public void removeUser(int data) throws SQLException {
-    try(PreparedStatement statement = conService.getConnection().prepareStatement(DELETE_USER)){
+    try{
+        conService.getConnection();
+        CallableStatement statement = conService.getConnection().prepareCall(DELETE_USER);
         statement.setInt(1,data);
 
         statement.executeUpdate();
-        }
+        }catch (Exception e){
+        e.getMessage();
+    }
     }
 
     @Override
