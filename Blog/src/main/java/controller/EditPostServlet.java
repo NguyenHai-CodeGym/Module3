0a7484ef922277585be75d94a33945dbf449bf5c
Hai.nguyen <ng.hai0603@gmail.com>
2021-02-22 14:12:37 +0700

    up

---
+++ Blog/src/main/java/controller/EditPostServlet.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "EditPostServlet")
+public class EditPostServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
